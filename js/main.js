ae265b9f988a79614a42236b2ff46061c85a4eb2
Samuel Martin <samuel.martin@dmh.se>
2017-03-09 22:15:11 +0100

    Switch tabs to spaces

--- js/main.js
+++ js/main.js
@@ -2,31 +2,31 @@ var imgs = $('.posterTrigger');
 var test = $('.concept-splash');
 
 for (var i = 0; i < imgs.length; i++) {
-	var rating,
-		title = $(imgs[i]).find('img')[0].alt;
+  var rating,
+    title = $(imgs[i]).find('img')[0].alt;
 
-	if (title in translated)
-		title = translated[title];
-	
-	$.ajax({
-		url: 'http://www.omdbapi.com/?t=' + title, 
-		success:
-		(function(container) {
-			return function(data) {
-				rating = data.imdbRating;
-				if (rating && rating != 'N/A') {
-					var imgURL = chrome.extension.getURL('img/star.png'),
-						imdbIMG = chrome.extension.getURL('img/imdb.png'),
-						imdbURL = 'http://www.imdb.com/title/' + data.imdbID,
+  if (title in translated)
+    title = translated[title];
+  
+  $.ajax({
+    url: 'http://www.omdbapi.com/?t=' + title, 
+    success:
+    (function(container) {
+      return function(data) {
+        rating = data.imdbRating;
+        if (rating && rating != 'N/A') {
+          var imgURL = chrome.extension.getURL('img/star.png'),
+            imdbIMG = chrome.extension.getURL('img/imdb.png'),
+            imdbURL = 'http://www.imdb.com/title/' + data.imdbID,
 
-						star = '<img class="imex-star" src="'+ imgURL +'" />',
-						span = '<span class="imex-bg">'+ rating +'</span>';
-						dblink = '<a target="_blank" class="imex-imdb" href="'+ imdbURL +'">' +
-								'<img class="imex-img" src="'+ imdbIMG +'" /></a>',
-					
-					container.append(star + span + dblink);
-				}
-			}
-		})($(imgs[i]))
-	});
+            star = '<img class="imex-star" src="'+ imgURL +'" />',
+            span = '<span class="imex-bg">'+ rating +'</span>';
+            dblink = '<a target="_blank" class="imex-imdb" href="'+ imdbURL +'">' +
+                '<img class="imex-img" src="'+ imdbIMG +'" /></a>',
+          
+          container.append(star + span + dblink);
+        }
+      }
+    })($(imgs[i]))
+  });
 }
