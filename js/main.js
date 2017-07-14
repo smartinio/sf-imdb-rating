4d719203e00e9d3040c9a6c24cbeb7b43bcf369d
smartin@swagbook <samuel.martin@dmh.se>
2017-07-14 16:17:15 +0200

    Fix errors and add animations

--- js/main.js
+++ js/main.js
@@ -13,37 +13,53 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
         return new DOMParser().parseFromString(response, 'text/html');
       },
 
-      formattedElement = rating => {
-        let str = `<p><img style="margin-top:-3px;vertical-align:middle;width:1em;height:1em" src="${starImage}"/>`;
+      formattedElement = (rating, title) => {
+        let str = `<p class="animated flipInX imdbRating"><img style="margin-top:-3px;vertical-align:middle;width:1em;height:1em" src="${starImage}"/>`;
             str += `<span style="color:white">&nbsp;${rating}</span></p>`;
         return $(str);
       },
 
-      parsePage = (response, container, title) => {
-        const rating = getDocument(response).getElementsByClassName('ratingValue')[0].children[0].innerText,
-              cacheEntry = {title, rating, date: new Date()},
+      parsePage = (response, container, title, imdbTitle) => {
+        const rateBox = getDocument(response).getElementsByClassName('ratingValue')[0],
+              rating = rateBox ? rateBox.children[0].innerText : 'N/A',
+              cacheEntry = {title, rating, date: new Date(), imdbTitle},
               cache = getCache();
 
         cache.push(cacheEntry);
         setCache(cache);
-        $(container).append(formattedElement(rating));
+        $(container).append(formattedElement(rating, imdbTitle));
       },
 
-      parseSearch = (response, container) => {
+      parseSearch = (response, container, longTitle, hasRating) => {
         const result = getDocument(response).getElementsByClassName('result_text')[0],
-              title = result.firstElementChild.href.split('/')[4],
-              cached = getCache().find(r => r.title === title);
+              title = '';
 
-        if (cached) $(container).append(formattedElement(cached.rating));
-        else chrome.runtime.sendMessage({ title, type: 'page' }, response => parsePage(response, container, title));
+        if (result) {
+          title = result.firstElementChild.href.split('/')[4];
+          chrome.runtime.sendMessage({ title, type: 'page' }, response => parsePage(response, container, longTitle, title));
+        }
+
+        else
+          if (!hasRating) $(container).append(formattedElement('N/A', title));
       },
 
       scan = () => {
         const containers = $('.ncgShowTitle').toArray().concat($('.ncgMovieTitle').toArray());
 
         for (let container of containers) {
-          const title = container.innerText;
-          chrome.runtime.sendMessage({ title, type: 'search' }, response => parseSearch(response, container));
+
+          const hasRating = $(container).is(':has(p.imdbRating)'),
+                title = hasRating? container.innerText.replace(/(\r\n|\n|\r)/gm, '').slice(0,-4) : container.innerText,
+                cached = getCache().find(r => r.title === title);
+
+          if (cached) {
+            if (!hasRating)
+              $(container).append(formattedElement(cached.rating, cached.imdbTitle));
+            continue;
+          }
+
+          else
+            chrome.runtime.sendMessage({ title, type: 'search' }, response => parseSearch(response, container, title, hasRating));
         }
       },
 
@@ -54,4 +70,5 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
       };
 
 setCache(getCache().filter(r => isNew(r.date)));
-document.addEventListener('aurelia-composed', function() { setTimeout(scan, 2000) }, true);
+$(document).ready(() => { setTimeout(scan, 2000) } );
+$('#Aurelia').change(() => { setTimeout(scan, 400) });
