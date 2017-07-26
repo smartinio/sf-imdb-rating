b62a62534f6f972033fc2862ab2e36c976262f38
smartin@swagbook <samuel.martin@dmh.se>
2017-07-26 15:49:26 +0200

    Add comments + some polish

--- js/main.js
+++ js/main.js
@@ -8,30 +8,35 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
       setCache = data => {
         localStorage.setItem('ratings', JSON.stringify(data));
       },
-    
+      
+      /* Used to generate a local copy of the DOM from IMDB */
       getDocument = response => {
         return new DOMParser().parseFromString(response, 'text/html');
       },
 
-      formattedElement = (rating, title) => {
-        let str = `<p class="animated flipInX imdbRating"><img style="margin-top:-3px;vertical-align:middle;width:1em;height:1em" src="${starImage}"/>`;
+      /* Used to generate the rating component on SF.se */
+      formattedElement = (rating) => {
+        let str = `<p class="animated flipInX imdbRating">`;
+            str += `<img style="margin-top:-3px;vertical-align:middle;width:1em;height:1em" src="${starImage}" />`;
             str += `<span style="color:white">&nbsp;${rating}</span></p>`;
         return $(str);
       },
 
       isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n),
 
-      parsePage = (response, container, title, imdbTitle) => {
+      /* Parses a movie's IMDB page and saves its rating */
+      parsePage = (response, container, title) => {
         const rateBox = getDocument(response).getElementsByClassName('ratingValue')[0],
               rating = rateBox ? rateBox.children[0].innerText : 'N/A',
-              cacheEntry = {title, rating, date: new Date(), imdbTitle},
+              cacheEntry = {title, rating, date: new Date()},
               cache = getCache();
 
         cache.push(cacheEntry);
         setCache(cache);
-        $(container).append(formattedElement(rating, imdbTitle));
+        $(container).append(formattedElement(rating));
       },
 
+      /* Parses the result of an IMDB search. Necessary because of swedish titles. */
       parseSearch = (response, container, longTitle, hasRating) => {
         const results = getDocument(response).getElementsByClassName('result_text'),
               currentYear = new Date().getFullYear(),
@@ -39,6 +44,7 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
 
         let result = false;
 
+        /* Only interested in movies from this, last or next year */
         outer:
         for (let year of years) {
           for (key in results) {
@@ -54,30 +60,32 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
 
         if (result) {
           title = result.firstElementChild.href.split('/')[4];
-          chrome.runtime.sendMessage({ title, type: 'page' }, response => parsePage(response, container, longTitle, title));
+          chrome.runtime.sendMessage({ title, type: 'page' }, response => parsePage(response, container, longTitle));
         }
 
-        else
-          if (!hasRating) $(container).append(formattedElement('N/A', longTitle));
+        else {
+          if (!hasRating) $(container).append(formattedElement('N/A'));
+        }
       },
 
+      /* Find the movie listings and start the score-retrieving process */
       scan = () => {
         const containers = $('.ncgShowTitle').toArray().concat($('.ncgMovieTitle').toArray());
 
         for (let container of containers) {
-
           const hasRating = $(container).is(':has(p.imdbRating)'),
                 title = hasRating? container.innerText.replace(/(\r\n|\n|\r)/gm, '').slice(0,-4) : container.innerText,
                 cached = getCache().find(r => r.title === title);
 
           if (cached) {
             if (!hasRating)
-              $(container).append(formattedElement(cached.rating, cached.imdbTitle));
+              $(container).append(formattedElement(cached.rating));
             continue;
           }
 
-          else
+          else {
             chrome.runtime.sendMessage({ title, type: 'search' }, response => parseSearch(response, container, title, hasRating));
+          }
         }
       },
 
@@ -87,6 +95,11 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
         return Math.abs(diff) < limit;
       };
 
+/*  Clear old cache entries */
 setCache(getCache().filter(r => isNew(r.date)));
+
+/*  Temporary solution to wait for aurelia to finish loading  */
 $(document).ready(() => { setTimeout(scan, 2000) } );
+
+/*  Re-scan on change (mainly tab-navigation)  */
 $('#Aurelia').change(() => { setTimeout(scan, 400) });
