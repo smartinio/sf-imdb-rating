df1082ab94a80302dc848cbb5903c4df5285c34c
Samuel Martin <samuel.martin@dmh.se>
2017-07-26 22:28:46 +0200

    Polish

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-const starImage = chrome.extension.getURL('img/icon48.png'),
-
-      getCache = () => {
+const getCache = () => {
         const cache = localStorage.getItem('ratings');
         return cache ? JSON.parse(cache) : [];
       },
@@ -9,22 +7,28 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
         localStorage.setItem('ratings', JSON.stringify(data));
       },
       
-      /* Used to generate a local copy of the DOM from IMDB */
+      /* Generates a local copy of the DOM from IMDB. */
       getDocument = response => {
         return new DOMParser().parseFromString(response, 'text/html');
       },
 
-      /* Used to generate the rating component on SF.se */
+      /* Creates a rating component for SF.se. */
       formattedElement = (rating) => {
-        let str = `<p class="animated flipInX imdbRating">`;
-            str += `<img style="margin-top:-3px;vertical-align:middle;width:1em;height:1em" src="${starImage}" />`;
-            str += `<span style="color:white">&nbsp;${rating}</span></p>`;
+        const starImage = chrome.extension.getURL('img/icon48.png'),
+              pClass = "animated flipInX imdbRating",
+              imgStyle = "margin-top:-3px; vertical-align:middle; width:1em; height:1em",
+              spanStyle = "color: white";
+
+        let str = `<p class="${pClass}">`;
+            str += `<img style="${imgStyle}" src="${starImage}" />`;
+            str += `<span style="${spanStyle}">&nbsp;${rating}</span></p>`;
+
         return $(str);
       },
 
       isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n),
 
-      /* Parses a movie's IMDB page and saves its rating */
+      /* Parses a movie's IMDB page and saves its rating. */
       parsePage = (response, container, title) => {
         const rateBox = getDocument(response).getElementsByClassName('ratingValue')[0],
               rating = rateBox ? rateBox.children[0].innerText : 'N/A',
@@ -38,29 +42,24 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
 
       /* Parses the result of an IMDB search. Necessary because of swedish titles. */
       parseSearch = (response, container, longTitle, hasRating) => {
-        const results = getDocument(response).getElementsByClassName('result_text'),
+        const possibleMovies = Array.from(getDocument(response).querySelectorAll('.result_text')),
               currentYear = new Date().getFullYear(),
-              years = [currentYear, currentYear+1, currentYear-1];
+              fromNow = (movie) => {
+                return   movie.innerText.includes(`(${currentYear})`)
+                      || movie.innerText.includes(`(${currentYear + 1})`)
+                      || movie.innerText.includes(`(${currentYear - 1})`);
+              };
 
-        let result = false;
-
-        /* Only interested in movies from this, last or next year */
-        outer:
-        for (let year of years) {
-          for (key in results) {
-            if (!isNumeric(key)) continue;
-            if (results[key].innerText.includes(`(${year})`)) {
-              result = results[key];
-              break outer;
-            }
-          }
-        }
-
-        let title = '';
+        /* We're only interested in movies from this, last or next year. */
+        const result = possibleMovies.find(fromNow);
 
         if (result) {
-          title = result.firstElementChild.href.split('/')[4];
-          chrome.runtime.sendMessage({ title, type: 'page' }, response => parsePage(response, container, longTitle));
+          const title = result.firstElementChild.href.split('/')[4];
+
+          chrome.runtime.sendMessage(
+            { title, type: 'page' },
+            response => parsePage(response, container, longTitle)
+          );
         }
 
         else {
@@ -68,13 +67,16 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
         }
       },
 
-      /* Find the movie listings and start the score-retrieving process */
+      /* Finds the movie listings and starts the score-retrieving process. */
       scan = () => {
-        const containers = $('.ncgShowTitle').toArray().concat($('.ncgMovieTitle').toArray());
+        const containers = $('.ncgShowTitle').toArray().concat($('.ncgMovieTitle').toArray()),
+              clearDebris = (container) => {
+                return container.innerText.replace(/(\r\n|\n|\r)/gm, '').slice(0,-4)
+              }
 
         for (let container of containers) {
           const hasRating = $(container).is(':has(p.imdbRating)'),
-                title = hasRating? container.innerText.replace(/(\r\n|\n|\r)/gm, '').slice(0,-4) : container.innerText,
+                title = hasRating ? clearDebris(container) : container.innerText,
                 cached = getCache().find(r => r.title === title);
 
           if (cached) {
@@ -83,21 +85,25 @@ const starImage = chrome.extension.getURL('img/icon48.png'),
             continue;
           }
 
-          chrome.runtime.sendMessage({ title, type: 'search' }, response => parseSearch(response, container, title, hasRating));
+          chrome.runtime.sendMessage(
+            { title, type: 'search' },
+            response => parseSearch(response, container, title, hasRating)
+          );
         }
       },
 
-      isNew = date => {
+      isLessThanOneDayOld = ({ date }) => {
         const diff = new Date().getTime() - new Date(date).getTime(),
-              limit = 1000 * 3600;
-        return Math.abs(diff) < limit;
+              limit = 24 * 3600 * 1000;
+              
+        return diff < limit;
       };
 
-/*  Clear old cache entries */
-setCache(getCache().filter(r => isNew(r.date)));
+/*  Clear old cache entries. */
+setCache(getCache().filter(entry => isLessThanOneDayOld(entry)));
 
-/*  Temporary solution to wait for aurelia to finish loading  */
+/*  Temporary solution to wait for aurelia to finish loading.  */
 $(document).ready(() => { setTimeout(scan, 2000) } );
 
-/*  Re-scan on change (mainly tab-navigation)  */
+/*  Re-scan on change (mainly tab-navigation).  */
 $('#Aurelia').change(() => { setTimeout(scan, 400) });
